4cbdb5b0c99f81871b2ac53e417316c524cf6725
Ben Leimberger <wbleimberger@gmail.com>
2020-04-12 17:53:45 -0400

    Mongodb collection formatting and adding mongo to go

--- server/server.go
+++ server/server.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"context"
+    "fmt"
+    "log"
 	"net/http"
+
 	"github.com/labstack/echo"
+
+	// "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // ----- Structs -----
@@ -14,10 +22,33 @@ type Order struct {
 func main() {
 	e := echo.New()
 
+	// Mongo setup
+	setupMongo()
+
+	// List endpoints
 	e.GET("/stock/:ticker", getPrice)
 	e.POST("/order", order)
 
+	// Start http server
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func setupMongo() {
+	clientOptions := options.Client().ApplyURI("mongodb://mongodb:27017")
+	
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
 }
 
 
